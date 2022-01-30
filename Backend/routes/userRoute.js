9e7fdea8e62fc12e6bb4ede7ebcf1ad1119eb156
Nabia-Sheikh <nabiasheikh2222@gmail.com>
2022-01-30 03:31:46 -0800

    more user routes

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -21,5 +21,24 @@ router.get("/", async (req, res) => {
       res.status(500).json({ message: error.message });
     }
   });
+
+  router.delete("/:id", async (req, res) => {
+    try {
+      const deletedUser = await Users.findByIdAndDelete(req.params.id);
+      res.json(deletedUser).status(200);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+  router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+      const user = await Users.findById(id);
+      res.json(user).status(200);
+    } catch (error) {
+      res.status(404).json({ message: "Book not found" });
+    }
+  });
   
 module.exports = router;
